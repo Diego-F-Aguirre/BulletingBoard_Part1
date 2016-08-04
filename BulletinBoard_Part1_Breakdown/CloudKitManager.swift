680d737f09e9f2a40c33a9ee0b2eae467a640a8e
Diego-F-Aguirre <diego3884@gmail.com>
2016-08-04 15:42:44 -0600

    Updated with NSNotificationCenter
    - implemented NSNotificationCenter to updated the table view when new messages are added
    - updated code that reloads the tableview once we post a notification
    - added notes breakdown

--- BulletinBoard_Part1_Breakdown/CloudKitManager.swift
+++ BulletinBoard_Part1_Breakdown/CloudKitManager.swift
@@ -11,17 +11,25 @@ import UIKit
 import CloudKit
 
 class CloudKitManager {
-    
+    // For our use of CloudKit here we're simply going to be using the default container with a public database
     let database = CKContainer.defaultContainer().publicCloudDatabase
     
+    // This function simply fetches the records that match a type we're looking for
     func fetchRecordsWithType(type: String, sortDescriptors: [NSSortDescriptor]? = nil, completion: ([CKRecord]?, NSError?) -> Void) {
+        // We create a query using CKQuery passing in the type and creatring a NSPredicate who's value is set to true to fetch all the items
         let query = CKQuery(recordType: type, predicate: NSPredicate(value: true))
+        // A CKQuery also needs a sort descriptor set at intialization time so we call the sortDescriptors method on the query and set the sortDescriptor function parameter to it
         query.sortDescriptors = sortDescriptors
-        
+        // Finally on the database we performQuery to fetch our records
+        // We pass the query we created previously and set the inZoneWithID to nil and for the completionHandler we pass in the completion which hopefully is an array of CKRecords
         database.performQuery(query, inZoneWithID: nil, completionHandler: completion)
     }
     
+    // This function simply saves a CKRecord to the database
     func saveRecord(record: CKRecord, completion: ((NSError?) -> Void) = {_ in }) {
+        // all we do is call the database and saveRecord which is a method available to us on CKDatabase
+        // Since we don't need to do anything with the CKRecord in the closure let's nullify it with _
+        // Finally since the method has a completion closure of NSError let's complete with an error in case we have one
         database.saveRecord(record) { (_, error) in
             completion(error)
         }
--- BulletinBoard_Part1_Breakdown/MessageListViewController.swift
+++ BulletinBoard_Part1_Breakdown/MessageListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// 1. Make sure to conform to the appropriate delegates and datasource. We're also going to specifically need to conform to the UITextFieldDelegate
 class MessageListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var tableView: UITableView!
@@ -16,34 +17,46 @@ class MessageListViewController: UIViewController, UITableViewDataSource, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        MessageController.sharedController.refresh { (_) in
-            self.tableView.reloadData()
-        }
+        let nc = NSNotificationCenter.defaultCenter()
+        nc.addObserver(self, selector: #selector(messagesWereUpdated), name: MessagesControllerDidRefreshNotification, object: nil)
     }
     
+    //2 . Let's send a message
     @IBAction func postButtonPressed(sender: AnyObject) {
+        // Check if your textfield has text
         guard let messageText = messageTextField.text where messageText.characters.count > 0 else { return }
-        
+        // resign the keyboard upon hitting the post button
         messageTextField.resignFirstResponder()
+        // Create a Message from the passed in text and today's current date
         let message = Message(messageText: messageText, date: NSDate())
+        // Call your postNewMessage func from the MessagesController and pass in your message
         MessageController.sharedController.postNewMessage(message)
         self.messageTextField.text = ""
-        MessageController.sharedController.refresh { (_) in
-            self.tableView.reloadData()
-        }
+        
     }
     
+    func messagesWereUpdated(notification: NSNotification) {
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.tableView?.reloadData()
+        })
+        
+    }
+    
+    // 3. Call the textFieldShouldReturn func and resign the keyboard
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         return messageTextField.resignFirstResponder()
     }
     
+    // 4. Fill out our DataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return MessageController.sharedController.messages.count
     }
     
+    // 4.5 Create a dateFormatter computed property which we'll call for our detailTextLabel
     let dateFormatter: NSDateFormatter = {
         let formatter = NSDateFormatter()
         formatter.dateStyle = .ShortStyle
+        // Have teh students look up what doesRelativeDateFormatting does
         formatter.doesRelativeDateFormatting = true
         formatter.timeStyle = .ShortStyle
         
